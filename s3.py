413a2c8c213e15ab0c480f3c7154657c6fce1e3a
Bo Zhu <zhubo03@gmail.com>
2012-03-31 03:59:41 +0000

    go back home

--- s3.py
+++ s3.py
@@ -4,15 +4,18 @@ from boto.s3.connection import S3Connection
 from boto.s3.key import Key
 
 
-def upload_to_s3(key_id, key_secret, bucket_name, file_list):
+def upload_to_s3(key_id, key_secret, bucket_name,
+        list_files, public_files=True):
+
     conn = S3Connection(key_id, key_secret)
     buck = conn.get_bucket(bucket_name)
 
-    for file_name in file_list:
+    for file_name in list_files:
         obj = Key(buck, file_name)
         with open(file_name) as fp:
             obj.set_contents_from_file(fp)
-        obj.make_public()
+        if public_files:
+            obj.make_public()
 
 if __name__ == '__main__':
     from setting import AWS_KEY_ID, AWS_KEY, S3_BUCKET
